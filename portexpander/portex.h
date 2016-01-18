9cf98f0998932c433c4670594ca4ed4a549a7e8d
toni <matzeton@googlemail.com>
2016-01-18 22:10:23 +0100

    mcp23s08 probe/write

--- portexpander/portex.h
+++ portexpander/portex.h
@@ -8,6 +8,27 @@
 static const char prefix[] = PRINTP;
 #define INFO(fmt, ...) printk(KERN_INFO "%s" fmt "\n", prefix, __VA_ARGS__)
 
+enum MCP23S08_ADDR {
+  MCP23S08_ADDR_00 = 0x0,
+  MCP23S08_ADDR_01 = 0x2,
+  MCP23S08_ADDR_02 = 0x4,
+  MCP23S08_ADDR_03 = 0x6,
+};
+
+enum MCP23S08_REGS {
+  MCP23S08_REG_IODIR   = 0x00,
+  MCP23S08_REG_IPOL    = 0x01,
+  MCP23S08_REG_GPINTEN = 0x02,
+  MCP23S08_REG_DEFVAL  = 0x03,
+  MCP23S08_REG_INTCON  = 0x04,
+  MCP23S08_REG_IOCON   = 0x05,
+  MCP23S08_REG_GPPU    = 0x06,
+  MCP23S08_REG_INTF    = 0x07,
+  MCP23S08_REG_INTCAP  = 0x08,
+  MCP23S08_REG_GPIO    = 0x09,
+  MCP23S08_REG_OLAT    = 0x0A,
+};
+
 
 int portex_sysfs_init(void);
 
--- portexpander/portex_spi.c
+++ portexpander/portex_spi.c
@@ -5,51 +5,105 @@
  */
 
 #include <linux/kernel.h>
-#include <linux/init.h>
-#include <linux/spi/spi.h>	// spi functions/macros
+#include <linux/module.h>
+#include <linux/of_device.h>
+#include <linux/spi/spi.h>
 
 #include "portex.h"
 
+#define MCP23_S_8_ID 0x0
 
-/* MCP23S08 device information */
-struct spi_board_info spi_device_info = {
-  .modalias = "portex", /* module name */
-  .max_speed_hz = 1000000, /* MCP23S08 has 1Mhz clock */
-  .bus_num = 0, /* unused -> spi_new_device does not need this */
-  .chip_select = 0, /* default -> 0 */
-  .mode = SPI_CS_HIGH, /* chip_select is high(flank) -> data is read/write */
+
+/* function prototypes */
+static int mcp23s08_probe(struct spi_device *spi);
+static int mcp23s08_remove(struct spi_device *spi);
+
+
+/* device structure for the kernel device table */
+static const struct spi_device_id mcp23s08_ids[] = {
+  { "mcp23s08", MCP23_S_8_ID },
+  { },
 };
-static const uint spi_bits_per_word = 8;
-/* spi device (MCP23S08) */
-static struct spi_device *spi_device;
-/* spi bus master */
-static struct spi_master *spi_master;
+MODULE_DEVICE_TABLE(spi, mcp23s08_ids);
 
+/* device match structure */
+/* (detect the device if plugged in) */
+static const struct of_device_id mcp23s08_spi_of_match[] = {
+  {
+    .compatible = "microchip,mcp23s08",
+    .data = (void *) MCP23_S_8_ID,
+  },
+};
+
+/* MCP23S08 device protocol information */
+static struct spi_driver mcp23s08_driver = {
+  .id_table =  mcp23s08_ids,
+  /* default linux driver device information */
+  .driver = {
+    .name           = "mcp23s08",
+    .owner          = THIS_MODULE,
+    .of_match_table = of_match_ptr(mcp23s08_spi_match),
+  },
+  /* callback functions for the mcp23s08 */
+  .probe  = mcp23s08_probe, // called by the spi master, when a spi device was found
+  .remove = mcp23s08_remove // replace found with removed
+};
+
+/*
+static int mcp23s08_read(unsigned int reg)
+{
+  return 0;
+}
+*/
+
+static int mcp23s08_write(enum MCP23S08_ADDR addr, enum MCP23S08_REGS reg, u8 value)
+{
+  u8 tx[3];
+
+  tx[0] = 0x40 | addr; // datasheet: controlbyte: 01000 A1 A0 R/W
+  tx[1] = reg;
+  tx[2] = value;
+  return 0;
+}
+
+static int mcp23s08_probe(struct spi_device *spi)
+{
+  const struct of_device_id *match = of_match_device(of_match_ptr(mcp23s08_spi_of_match), &spi->dev);
+  int status, type;
+  u32 spi_present_mask = 0;
+
+  if (match) {
+    type   = (int)(uintptr_t)match->data;
+    /* check if mcp23s08 device mask available */
+    status = of_property_read_u32(spi->dev.of_node, "microchip,spi-present-mask", &spi_present_mask);
+    if (status) {
+      dev_err(&spi->dev, "DeviceTree has no spi-present-mask\n");
+      return -ENODEV;
+    }
+    /* check if mcp23s08 device mask valid */
+    if ((spi_present_mask <= 0) || (spi_present_mask >= 256)) {
+      dev_err(&spi->dev, "invalid spi-present-mask\n");
+      return -ENODEV;
+    }
+  }
+
+  dev_dbg(&spi->dev, "probing done\n");
+  mcp23s08_write(MCP23S08_ADDR_00, MCP23S08_REG_GPIO, 0xFF);
+  return 0;
+}
+
+static int mcp23s08_remove(struct spi_device *spi)
+{
+  dev_dbg(&spi->dev, "remove\n");
+  return 0;
+}
 
 int portex_spi_init(void)
 {
-  int ret;
-
-  if ( (spi_master = spi_busnum_to_master( spi_device_info.bus_num )) == 0 ) {
-    INFO("spi_busnum_to_master(%d) failed", spi_device_info.bus_num);
-    return -ENODEV;
-  }
-  INFO("bus_master; bus_num: %d; mode: %u; min_speed: %u; max_speed: %u; bits_per_word: %u", spi_master->bus_num, spi_master->mode_bits, spi_master->min_speed_hz, spi_master->max_speed_hz, spi_master->bits_per_word_mask);
-  if ( (spi_device = spi_new_device( spi_master, &spi_device_info )) == 0 ) {
-    INFO("%s", "spi_new_device(...) failed");
-    return -ENODEV;
-  }
-  spi_device->bits_per_word = spi_bits_per_word;
-  if ( (ret = spi_setup(spi_device)) != 0 ) {
-    spi_unregister_device(spi_device);
-    return ret;
-  }
-  return 0;
+  return spi_register_driver(&mcp23s08_driver);
 }
 
 void portex_spi_free(void)
 {
-  spi_unregister_device(spi_device);
+  spi_unregister_driver(&mcp23s08_driver);
 }
-
-//spi_write( spi_device, &write_data, sizeof write_data );
